47e699be580fcf11c7f81d6e5ce2d837db65d355
Wentrick <davi.w.feijo@gmail.com>
2023-04-24 14:10:42 -0300

    questao 1

---
+++ listas/lista1/lista1_script.R
@@ -0,0 +1,24 @@
+pacman::p_load(readr,tidyverse,stringr)
+
+##### Questao 1 ----
+
+alfabeto = c("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z")
+
+dicionario_ptbr <- read_csv("dados/dicionario_ptbr.txt") %>%
+  select(palavras = 1)
+
+matches = c()
+for (n in 1:10) {
+  amostra = c()
+  for (i in 1:50000) {
+    palavra[i] = str_flatten(paste(sample(alfabeto,5,replace = TRUE)))
+    amostra[i] = palavra[i]
+  }
+match[n] = sum(dicionario_ptbr$palavras %in% amostra)
+  
+matches[n] = match[n]
+}
+
+matches
+
+
